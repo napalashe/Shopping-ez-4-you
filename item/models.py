37444c976e29fe2306fb3cd5cab58b8a7834b8d7
napalashe <irunatbears@gmail.com>
2024-09-12 00:48:07 -0700

    created item app

---
+++ item/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+    
+class Item(models.Model):
+    catagory = models.ForeignKey(Category, related_name='items',on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images',blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='items', on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    deleted_at=models.DateTimeField(auto_now_add=True)
